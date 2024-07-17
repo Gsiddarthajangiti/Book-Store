58ee74015de614dfbd57b0461b7d6cd1db2442d2
Gowtham Siddartha <gowthamjangiti5@gmail.com>
2024-07-17 20:15:42 +0530

    first commit

---
+++ backend/config.js
@@ -0,0 +1,2 @@
+export const PORT=5555;
+export const mongoDBURL = 'mongodb+srv://gowthamjangiti4:gowthamjangiti4@bcoding.pkafhel.mongodb.net/books-collection?retryWrites=true&w=majority&appName=Bcoding'
